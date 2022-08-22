b281acd6d6e2fe19a2c2ac7e3396f86a289c00e0
Danilo Martins <dansm86@outlook.com>
2022-08-22 20:15:13 -0300

    req20-render dos botões de busca por categorias

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,37 +6,79 @@ function Recipes() {
   const history = useHistory();
   const { location: { pathname } } = history;
   const [recipes, setRecipes] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const MAX_LENGTH = 12;
-
+  const [categories, setCategories] = useState([]);
+  const [isLoadingRecipes, setIsLoadingRecipes] = useState(true);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const MAX_LENGTH_RECIPES = 12;
+  const MAX_LENGTH_CATEGORIES = 5;
+  // data-testid=${categoryName}-category-filter
+  // meal
+  // https://www.themealdb.com/api/json/v1/1/list.php?c=list
+  // drink
+  // https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list
   useEffect(() => {
     if (pathname === '/foods') {
       fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
         .then((response) => response.json())
         .then((results) => {
           const filteredResults = results.meals
-            .filter((_, index) => index < MAX_LENGTH);
+            .filter((_, index) => index < MAX_LENGTH_RECIPES);
           setRecipes(filteredResults);
         })
         .catch((err) => console.log(err.message))
-        .finally(() => setIsLoading(false));
+        .finally(() => setIsLoadingRecipes(false));
+
+      fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+        .then((response) => response.json())
+        .then((results) => {
+          const filteredResults = results.meals
+            .filter((_, index) => index < MAX_LENGTH_CATEGORIES);
+          setCategories(filteredResults);
+        })
+        .catch((err) => console.log(err.message))
+        .finally(() => setIsLoadingCategories(false));
     } else {
       fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
         .then((response) => response.json())
         .then((results) => {
           const filteredResults = results.drinks
-            .filter((_, index) => index < MAX_LENGTH);
+            .filter((_, index) => index < MAX_LENGTH_RECIPES);
           setRecipes(filteredResults);
         })
         .catch((err) => console.log(err.message))
-        .finally(() => setIsLoading(false));
+        .finally(() => setIsLoadingRecipes(false));
+
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+        .then((response) => response.json())
+        .then((results) => {
+          const filteredResults = results.drinks
+            .filter((_, index) => index < MAX_LENGTH_CATEGORIES);
+          setCategories(filteredResults);
+        })
+        .catch((err) => console.log(err.message))
+        .finally(() => setIsLoadingCategories(false));
     }
   }, []);
-
+  // data-testid=${categoryName}-category-filter
   return (
     <div>
       {
-        !isLoading && recipes.map((recipe, index) => (
+        !isLoadingCategories && <button type="button">All</button>
+      }
+      {
+        !isLoadingCategories && categories.map((category, index) => (
+          <button
+            key={ index }
+            type="button"
+            data-testid={ `${category.strCategory}-category-filter` }
+          >
+            { category.strCategory }
+
+          </button>
+        ))
+      }
+      {
+        !isLoadingRecipes && recipes.map((recipe, index) => (
           <FoodsCard
             key={ recipe.idMeal || recipe.idDrink }
             recipes={ recipe }
