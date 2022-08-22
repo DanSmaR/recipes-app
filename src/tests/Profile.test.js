d6551cd30d0ec7bce3fb6174895db93cf36c6c09
Felipe <fe.donatto@outlook.com>
2022-08-22 16:26:35 -0300

    req 57-62 concluidos

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Profile from '../pages/Profile';
+import renderWithRouter from './helpers/renderWithRouter';
+
+
+describe('Testa o componente FOOTER', () => {
+  it('Renderiza todos os elementos corretamente', () => {
+    renderWithRouter(<Profile />)
+    localStorage.setItem('user', {email: 'email@email.com'})
+    screen.logTestingPlaygroundURL()
+    const email = screen.getByTestId('profile-email')
+    const doneRecipes = screen.getByRole('button', {
+        name: /done recipes/i
+      })
+    const favoriteRecipes = screen.getByRole('button', {
+        name: /favorite recipes/i
+      })
+    const logout = screen.getByRole('button', {
+        name: /logout/i
+      })
+
+    expect(email).toBeInTheDocument()
+    expect(doneRecipes).toBeInTheDocument()
+    expect(favoriteRecipes).toBeInTheDocument()
+    expect(logout).toBeInTheDocument()
+    userEvent.click(logout)
+    console.log(localStorage)
+    expect(localStorage.getItem('user')).toBeNull()
+
+  });
+  
+});
