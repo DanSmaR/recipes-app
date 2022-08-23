752c52f556764bc50287021665d0a84313964e86
Danilo Martins <dansm86@outlook.com>
2022-08-23 02:56:54 -0300

    req19-23-refatorado fetch das receitas

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,72 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from './Card';
+import { URLMealsArr, URLDrinksArr } from '../utils/constants';
 
 function Recipes() {
   const history = useHistory();
   const { location: { pathname } } = history;
   const [recipes, setRecipes] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [isLoadingRecipes, setIsLoadingRecipes] = useState(true);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const MAX_LENGTH_RECIPES = 12;
   const MAX_LENGTH_CATEGORIES = 5;
-  // data-testid=${categoryName}-category-filter
-  // meal
-  // https://www.themealdb.com/api/json/v1/1/list.php?c=list
-  // drink
-  // https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list
+
+  function handleFetchAll(typeOfRecipe, UrlArr) {
+    Promise.all(UrlArr.map((url) => fetch(url)))
+      .then((responses) => Promise.all(responses.map((res) => res.json())))
+      .then((results) => {
+        console.log(results);
+        const filteredResults = [
+          results[0][typeOfRecipe].filter((_, index) => index < MAX_LENGTH_RECIPES),
+          results[1][typeOfRecipe].filter((_, index) => index < MAX_LENGTH_CATEGORIES),
+        ];
+        setRecipes(filteredResults[0]);
+        setCategories(filteredResults[1]);
+      })
+      .catch((err) => console.log(err.message))
+      .finally(() => setIsLoading(false));
+  }
+
   useEffect(() => {
     if (pathname === '/foods') {
-      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-        .then((response) => response.json())
-        .then((results) => {
-          const filteredResults = results.meals
-            .filter((_, index) => index < MAX_LENGTH_RECIPES);
-          setRecipes(filteredResults);
-        })
-        .catch((err) => console.log(err.message))
-        .finally(() => setIsLoadingRecipes(false));
-
-      fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-        .then((response) => response.json())
-        .then((results) => {
-          const filteredResults = results.meals
-            .filter((_, index) => index < MAX_LENGTH_CATEGORIES);
-          setCategories(filteredResults);
-        })
-        .catch((err) => console.log(err.message))
-        .finally(() => setIsLoadingCategories(false));
-    } else {
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-        .then((response) => response.json())
-        .then((results) => {
-          const filteredResults = results.drinks
-            .filter((_, index) => index < MAX_LENGTH_RECIPES);
-          setRecipes(filteredResults);
-        })
-        .catch((err) => console.log(err.message))
-        .finally(() => setIsLoadingRecipes(false));
-
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
-        .then((response) => response.json())
-        .then((results) => {
-          const filteredResults = results.drinks
-            .filter((_, index) => index < MAX_LENGTH_CATEGORIES);
-          setCategories(filteredResults);
-        })
-        .catch((err) => console.log(err.message))
-        .finally(() => setIsLoadingCategories(false));
-    }
+      handleFetchAll('meals', URLMealsArr);
+    } else handleFetchAll('drinks', URLDrinksArr);
   }, []);
-  // data-testid=${categoryName}-category-filter
+
   return (
     <div>
       {
-        !isLoadingCategories && <button type="button">All</button>
+        !isLoading && <button type="button">All</button>
       }
       {
-        !isLoadingCategories && categories.map((category, index) => (
+        !isLoading && categories.map((category, index) => (
           <button
             key={ index }
             type="button"
@@ -79,7 +53,7 @@ function Recipes() {
       }
       <ul>
         {
-          !isLoadingRecipes && recipes.map((recipe, index) => (
+          !isLoading && recipes.map((recipe, index) => (
             <Card
               key={ recipe.idMeal || recipe.idDrink }
               recipes={ recipe }
@@ -92,10 +66,5 @@ function Recipes() {
     </div>
   );
 }
-
-// meals
-// https://www.themealdb.com/api/json/v1/1/search.php?s=
-// drinks
-// https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
 
 export default Recipes;
