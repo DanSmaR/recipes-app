9320aa2b83d0b5d67fb51875c84085e97ec3e586
Wellington Ferreira <wantunesferreira@gmail.com>
2022-08-26 00:26:51 -0300

    atualizado arquivo com remoto

--- src/utils/mocks/doneRecipesLocalStorage.js
+++ src/utils/mocks/doneRecipesLocalStorage.js
@@ -44,7 +44,6 @@ export const favoriteRecipes = [
   },
 ];
 
-<<<<<<< HEAD
 export const inProgressFoodRecipes = {
   meals: {
     52771: [],
@@ -57,6 +56,4 @@ export const inProgressDrinkRecipes = {
   },
 };
 
-=======
->>>>>>> 18abffda40519ff382fdd65cef356cef6d2b4b04
 export default doneRecipes;
